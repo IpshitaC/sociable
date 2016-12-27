a440278609ac10952532d74cfb77a4e22a1f1c32
Ipshita Chatterjee <chatterjeei08@gmail.com>
2016-12-27 12:44:07 +0530

    chore:enhanced readability

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 Sociable is a personality insights based web-application which gives detailed insights of a person's characteristics, based on the analysis of a body of text written by them.
 This tool is designed for the use of HR Managers and other hiring professionals, who need to determine the applicant's character, personality and compatibility to the organisation, often without meeting them in person. This app gives detailed and accurate insights, based on the sample text written by the applicants.
 
-This application was built during the #IBM include < geek.tech > Hackathon, Delhi. 
+This application was built during the #IBM include < geek.tech > Hackathon, Delhi.
 
 ## How does it work?
 We need a minimum of 3500 words and ideally 6000 words or more to compute statistically significant results. [Read more](http://www.ibm.com/watson/developercloud/doc/personality-insights/science.shtml)
--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2015 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
 var express    = require('express'),
@@ -31,8 +15,8 @@ require('./config/express')(app);
 // Create the service wrapper
 var personalityInsights = watson.personality_insights({
   version: 'v2',
-  username: '<username>',
-  password: '<password>'
+  username: '<username>', //Replace this with your username
+  password: '<password>' //Replace this with your password
 });
 
 app.get('/', function(req, res) {
