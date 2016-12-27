b426ed50e67867a3bb744c88a6b9283545187b67
Ipshita Chatterjee <chatterjeei08@gmail.com>
2016-12-27 12:23:59 +0530

    chore:updated directory structure

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,19 +1,4 @@
-/**
- * Copyright 2015 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- /* global i18nProvider,i18nTranslatorFactory,grecaptcha,d3,textSummary */
+/* global i18nProvider,i18nTranslatorFactory,grecaptcha,d3,textSummary */
 
 'use strict';
 
@@ -60,7 +45,7 @@ $(document).ready(function() {
   $content.change(updateWordsCount);
 
   /**
-   * Update words count on copy/past
+   * Update words count on copy/paste
    */
   $content.bind('paste', function() {
     setTimeout(updateWordsCount, 100);
